162bcd89f75d28c96e496aff08b9c84df1c70add
giventusmvh <weishen735@gmail.com>
2024-02-21 19:51:45 +0700

    add passport package, add artikels seeder

---
+++ database/seeders/ArtikelSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Artikel;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ArtikelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Artikel::table('artikels')->insert(
+            [
+                'judul' => 'Dampak Toxic Parents pada Psikologi Anak',
+                'narasi' => 'Banyak orang tua yang menjadi toxic parent dikarenakan tidak mengerti bagaimana dampak terhadap buah hatinya. Para pelaku menganggap apa yang dilakukannya adalah hal yang baik dan membangun mental anak. Dengan mengerti dampak toxic parent maka kita akan dapat menghentikan kesalahan dalam mendidik. 1. Dampak Emosional Anak-anak yang tumbuh dalam lingkungan dengan orang tua toksik rentan mengalami gangguan emosional seperti kecemasan, depresi, dan rendahnya rasa percaya diri. Penelitian yang dilakukan oleh Johnson dan Green (2018) menunjukkan bahwa interaksi yang didominasi oleh kritik, manipulasi, atau kontrol dapat merusak keseimbangan emosional anak. 2. Kurangnya Dukungan Emosional Orang tua toksik sering kali tidak memberikan dukungan emosional yang diperlukan anak. Smith dan Jones (2019) menyimpulkan dalam penelitiannya bahwa kurangnya dukungan ini dapat mengganggu perkembangan emosional anak. 3. Gangguan dalam Perkembangan Sosial Anak-anak yang tumbuh dengan orang tua toksik mungkin mengalami hambatan dalam mengembangkan keterampilan sosial yang sehat. Penelitian oleh Brown dan rekan (2020) menunjukkan bahwa pola hubungan yang berkonflik dan merugikan dapat menghambat kemampuan anak berinteraksi dengan teman sebaya.',
+                'image' => '20231018123621.jpg',
+            ],
+            [
+                'judul' => 'Sering Cemas? Yuk, Kenalan dengan Anxiety',
+                'narasi' => 'Kecemasan sering muncul dan sewajarnya pernah dirasakan semua orang, baik itu laki-laki serta perempuan. Cemas merupakan perasaan wajar yang dapat dialami oleh seluruh oang di dunia. Namun yang perlu diperhatikan, jika kamu mengalami kecemasan secara berlebihan yang hampir membuatmu sudah tidak nyaman dan mengganggu aktifitas keseharian. Di moment tersebut mungkin sudah saatnya kamu berkenalan dengan “Apa itu Anxiety?”. Apa Itu Anxiety? Definisi anxiety atau biasa kita sebut kecemasan adalah kondisi kesehatan mental umum yang dapat mempengaruhi siapa saja, tanpa memandang usia, jenis kelamin, atau latar belakang. Meskipun kecemasan adalah respon alami terhadap situasi stres, terlalu banyak kecemasan dapat menyebabkan gangguan kecemasan yang mempengaruhi kualitas hidup dan kesejahteraan seseorang. Pengertian lain dari anxiety yaitu Kecemasan adalah keadaan emosional yang kompleks, ditandai oleh perasaan cemas, takut, dan gelisah. Hal ini normal terjadi dalam kehidupan sehari-hari, namun, ketika kecemasan menjadi berlebihan dan mengganggu fungsi sehari-hari, dapat menjadi tanda adanya gangguan kecemasan. Untuk dapat mendiaknosis apakah kita mengalami gangguan anxiety maka perlu bantuan tenaga profesional, sehingga diaknosis bisa dilanjutkan dengan tindak lanjut yang tepat. Pemahaman mengenai gangguan anxiety dapat mempercepat pemulihan, sehingga diharapkan kita dapat mengerti gejalanya sebelum benar - benar menjadi gangguan anxiety. Jangan tunggu mental kita mengalami kecemasan yang berlebih barulah kita mencari pertolongan. Berbagi cerita kepada orang yang tepat akan membuat diri jauh merasa lebih nyaman. Namun bila bingung mencari teman bercerita yang membuat nyaman dan aman',
+                'image' => '20231018123748.jpg',
+            ],
+            [
+                'judul' => 'Strategi Menghadapi Toxic Parents',
+                'narasi' => 'Saat ini banyak sekali para remaja atau bahkan orang dewasa yang terganggu mentalnya dikarenakan fenomena toxic parent. Bukanlah hal yang mudah dalam menghadapi kondisi yang mengerikan dan membuat trauma, namun ada beberapa hal yang dapat dilakukan dalam menghadapi toxic parent, beikut diantaranya : Pendidikan dan Kesadaran Langkah pertama adalah meningkatkan pemahaman anak mengenai karakteristik dan perilaku orang tua toksik. Jones dan koleganya (2021) menekankan bahwa pengetahuan ini akan membantu anak mengenali tanda-tanda perilaku merugikan dan mengatasi dampaknya. Menetapkan Batasan Penting bagi anak untuk memahami pentingnya menetapkan batasan dalam hubungan dengan orang tua toksik. White dan Davis (2022) merekomendasikan agar anak mengenali perilaku yang merugikan dan mengambil langkah untuk melindungi diri. Dukungan Psikologis Terapi psikologis dapat membantu anak mengatasi dampak negatif dari orang tua toksik. Thompson dan koleganya (2020) menunjukkan bahwa terapi perilaku kognitif efektif dalam mengurangi tingkat kecemasan dan depresi akibat pola hubungan yang tidak sehat. Nah, berikut kesimpulan yang dapat kita ambil pelajarannya salah satunya yaitu Keterlibatan orang tua dalam membentuk kesejahteraan anak memegang peranan penting, tetapi relasi yang beracun dapat menghasilkan konsekuensi serius pada perkembangan mereka. Efek emosional, kekurangan dukungan, dan hambatan perkembangan sosial bisa diatasi melalui edukasi, pembatasan yang jelas, dan sokongan psikologis yang tepat. Mengenali serta memahami pola perilaku orang tua yang merugikan merupakan langkah pertama yang signifikan bagi anak dalam mengatasi dampak negatifnya. Dengan memperoleh pengetahuan, meningkatkan kesadaran, dan memberikan dukungan sesuai kebutuhan, kita bisa membantu anak-anak menghadapi cobaan yang mungkin mereka hadapi dalam konteks hubungan keluarga yang tak sehat. Prioritas tertinggi adalah kesejahteraan anak, dan melalui usaha bersama, kita bisa mendorong mereka untuk tumbuh dan berkembang dalam lingkungan yang lebih positif serta mendukung. Dengan memahami implikasi serta menerapkan langkah-langkah penanganan yang sesuai, kita berpotensi membentuk masa depan yang lebih cerah bagi generasi mendatang. Temukan serta pelajari solusi praktis lainnya yang bisa kamu dapatkan hanya di website ini. Mari bersama-sama menciptakan masa depan yang lebih positif bagi anak-anak kita. Ingin berdiskusi mengenai buah hati',
+                'image' => '20231018123824.jpg',
+            ],
+            [
+                'judul' => 'Toxic Parents Monster untuk Masa Depan Anak',
+                'narasi' => 'Dalam dinamika keluarga, peran orang tua memiliki dampak yang signifikan pada perkembangan anak. Namun, terkadang pola hubungan yang tidak sehat antara orang tua dan anak dapat membawa dampak negatif yang serius. Konsep ini dikenal sebagai "Orang Tua Toksik" atau orang tua yang memiliki perilaku dan pola pikir yang merugikan anak. Dalam artikel ini, kami akan menjelaskan bagaimana mengerikannya orang tua toksik terhadap anak sehingga kita mampu menghindar dari predikat tersebut dan juga bisa menjaga lingkungan sekitar dari hal mengerikan ini. Definisi “Toxic Parents" atau dalam bahasa Indonesia disebut "Orang Tua Toksik" mengacu pada pola perilaku dan relasi yang membahayakan antara orang tua dan anak. Istilah ini dipakai untuk mengilustrasikan orang tua yang secara terus-menerus atau berulang kali menampilkan tindakan yang tidak sehat, merugikan, atau merendahkan anak dari segi emosi, psikologis, atau bahkan fisik. Pola interaksi ini bisa berdampak negatif jangka panjang pada pertumbuhan serta kesejahteraan anak, termasuk gangguan dalam aspek emosional, mental, dan sosial. Hal tersebut bisa berawal dari hal kecil yang tidak disadari oleh orang sekitar. Bila saat ini kita melihat atau justru menjadi pelaku atau pun korban, mulailah untuk menyudahi cerita toxic parent ini dengan memulai dari diri sendiri. Ingin berdiskusi mengenai buah hati',
+                'image' => '20231018123845.jpg',
+            ],
+    );
+    }
+}
