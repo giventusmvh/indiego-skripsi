5148ea195a37ae27cba3cd54c01cb2a9a1acaaed
giventusmvh <weishen735@gmail.com>
2024-04-04 14:32:47 +0700

    add form schedule

--- resources/views/konselor/test.blade.php
+++ resources/views/konselor/test.blade.php
@@ -79,33 +79,85 @@
   
   <!-- NavBar End -->
 
-  <section class="md:max-w-[1200px] mx-auto w-[90%] mt-[120px] mb-[100px] flex flex-col justify-center items-center">
-    
-        <p>Tambah Jadwal Konseling</p>
-        <form class="md:w-[80%] w-[90%] mx-auto ">
-            <div class="mb-5">
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+  <section class="md:max-w-[1200px] mx-auto w-[90%] mt-[120px] mb-[100px]">
+    <div class="flex md:flex-row md:gap-[50px]">
+        <div class="flex flex-col gap-[20px]">
+            <a href="" class="bg-slate-300 w-full h-[60px] flex items-center justify-center p-[30px] shadow-md border-slate-400 border-[1px]">
+                <p>Add Schedule</p>
+            </a>
+            <hr>
+            <a href="" class="bg-slate-300 w-full h-[60px] flex items-center justify-center p-[30px] shadow-md border-slate-400 border-[1px]">
+                <p>All</p>
+            </a>
+            <a href="" class="bg-slate-300 w-full h-[60px] flex items-center justify-center p-[30px] shadow-md border-slate-400 border-[1px]">
+                <p>Booked</p>
+            </a>
+            <a href="" class="bg-slate-300 w-full h-[60px] flex items-center justify-center text-center p-[30px] shadow-md border-slate-400 border-[1px]" >
+                <p>Pending Confirmation</p>
+            </a>
+            <a href="" class="bg-slate-300 w-full h-[60px] flex items-center justify-center text-center p-[30px] shadow-md border-slate-400 border-[1px]">
+                <p>Reschedule Confirmation</p>
+            </a>
+        </div>
+        <div class="flex flex-col w-full gap-[30px]">
+            <div class="flex flex-col md:flex-row gap-[30px]">
+                <div class=" bg-slate-300 w-full  flex  p-[30px] shadow-md border-slate-400 border-[1px] flex-col">
+                    <p>Tanggal :</p>
+                    <p>Jam :</p>
+                    <p>Topik Konseling :</p>
+                    <p>Tipe Konseling :</p>
+                    <p>Status :</p>
+
+                    <div class="mt-[30px] flex items-end justify-end">
+                        <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Green</button>
+                        <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Red</button>
+                    </div>
+                </div>
+
+                <div class="bg-slate-300 w-full  flex  p-[30px] shadow-md border-slate-400 border-[1px] flex-col">
+                    <p>Tanggal :</p>
+                    <p>Jam :</p>
+                    <p>Topik Konseling :</p>
+                    <p>Tipe Konseling :</p>
+                    <p>Status :</p>
+
+                    <div class="mt-[30px] flex items-end justify-end">
+                        <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Green</button>
+                        <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Red</button>
+                    </div>
+                </div>
             </div>
-            <div class="mb-5">
-                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-                <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+
+
+            <div class="flex flex-col md:flex-row gap-[30px]">
+                <div class=" bg-slate-300 w-full  flex  p-[30px] shadow-md border-slate-400 border-[1px] flex-col">
+                    <p>Tanggal :</p>
+                    <p>Jam :</p>
+                    <p>Topik Konseling :</p>
+                    <p>Tipe Konseling :</p>
+                    <p>Status :</p>
+
+                    <div class="mt-[30px] flex items-end justify-end">
+                        <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Green</button>
+                        <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Red</button>
+                    </div>
+                </div>
+
+                <div class="bg-slate-300 w-full  flex  p-[30px] shadow-md border-slate-400 border-[1px] flex-col">
+                    <p>Tanggal :</p>
+                    <p>Jam :</p>
+                    <p>Topik Konseling :</p>
+                    <p>Tipe Konseling :</p>
+                    <p>Status :</p>
+
+                    <div class="mt-[30px] flex items-end justify-end">
+                        <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Green</button>
+                        <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Red</button>
+                    </div>
+                </div>
             </div>
-            <div class="mb-5">
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
-            </div>
-            <div class="mb-5">
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
-            </div>
-            <div class="mb-5">
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
-            </div>
-            <button type="submit" class="w-full min-w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-        </form>
-  
+        </div>
+    </div>
   </section>
 
   
